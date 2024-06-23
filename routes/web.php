701fa3216a2ec63eba50ce934b4b77a87b7a7e6c
devprojectsagadir <149078568+devprojectsagadir@users.noreply.github.com>
2024-06-23 16:13:53 +0100

    Revert "testing redirect"
    This reverts commit 26f969803c60bb7d1726c8de83fce62375005985.

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/public', function () {
+    return view('welcome');
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
