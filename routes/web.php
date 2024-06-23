497564831c259d8e614f72d293a4b4f958e69aca
devprojectsagadir <149078568+devprojectsagadir@users.noreply.github.com>
2024-06-23 15:46:17 +0100

    adding breeze auth
    yarbi ykhdam

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-Route::get('/public', function () {
-    return redirect('/');
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::get('/login', function () {
-    return "test";
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
